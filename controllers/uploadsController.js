676d715d5973b2bd2146313ece8ad579c1528190
liansun96 <liansun0203@gmail.com>
2024-09-25 21:49:22 +0630

    updated

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -3,15 +3,21 @@ const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
 const cloudinary = require('cloudinary').v2;
 const fs = require('fs');
+const { log } = require('console');
 
-const uploadProductImageLocal = async (req, res) => {
+const uploadProductImageLocal = async (req, res) => {   
   if (!req.files) {
     throw new CustomError.BadRequestError('No File Uploaded');
   }
+  // console.log(req.files);
+  
   const productImage = req.files.image;
   if (!productImage.mimetype.startsWith('image')) {
     throw new CustomError.BadRequestError('Please Upload Image');
   }
+  // console.log(productImage, productImage.mimetype);
+  
+
   const maxSize = 1024 * 1024;
   if (productImage.size > maxSize) {
     throw new CustomError.BadRequestError('Please upload image smaller 1MB');
